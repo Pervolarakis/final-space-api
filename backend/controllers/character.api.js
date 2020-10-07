8dad39c791151582bac10f605a70d94658b518da
Your Name <mihalaras11@gmail.com>
2020-10-07 11:26:08 +0300

    added limit param

--- backend/controllers/character.api.js
+++ backend/controllers/character.api.js
@@ -1,16 +1,19 @@
 const Character = require("./../models/character.model");
 
 const getAllCharacters = async (req, res) => {
-  await Character.find({}, (err, characters) => {
-    if (err) {
-      return res.status(500).send({
-        success: false,
-        message: "Something went wrong.",
-      });
-    }
-    res.set("Cache-Control", "public, max-age=315576");
-    return res.json(Character.structure(characters));
-  });
+  console.log(req.query.limit)
+  await Character
+    .find({}, (err, characters) => {
+      if (err) {
+        return res.status(500).send({
+          success: false,
+          message: "Something went wrong.",
+        });
+      }
+      res.set("Cache-Control", "public, max-age=315576");
+      return res.json(Character.structure(characters));
+    })
+    .limit((req.query.limit)? parseInt(req.query.limit): 0);
 };
 
 const getSingleCharacter = async (req, res) => {
